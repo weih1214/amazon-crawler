247fabbf7f449b27b51ea216613457bfaa1a307c
hwei <weih1214@gmail.com>
2016-12-15 14:24:41 +0800

    Create AnswerComment class and corresponding page processor; Reformat some previous work.

---
+++ crawler/src/main/java/sg/edu/smu/webmining/crawler/datatype/AnswerComment.java
@@ -0,0 +1,108 @@
+package sg.edu.smu.webmining.crawler.datatype;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by hwei on 14/12/2016.
+ */
+public class AnswerComment {
+
+  private static final Pattern ANSWERCOMMENT_ID_PATTERN = Pattern.compile("_(.*)");
+  private static final Pattern AUTHOR_ID_PATTERN = Pattern.compile("/profile/([a-zA-Z0-9]+)/");
+  private static final Pattern DATE_PATTERN = Pattern.compile("((January|February|March|April|May|June|July|August|September|October|November|December) \\d\\d? \\d{4})");
+
+  private static final DateFormat DATE_FORMAT;
+
+  static {
+    DATE_FORMAT = new SimpleDateFormat("MMMM d yyyy");
+    DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("PDT"));
+  }
+
+  private final Element answerCommentElement;
+  private final String questionId;
+  private final String answerText;
+
+  public AnswerComment(Element answerCommentElement, String questionId, String answerText) {
+    this.answerCommentElement = answerCommentElement;
+    this.questionId = questionId;
+    this.answerText = answerText;
+  }
+
+  public String getAnswerCommentId() {
+    final Elements elements = answerCommentElement.select("div.cdCommentText span");
+    if (!elements.isEmpty()) {
+      final String AnswerCommentIdString = elements.first().attr("id");
+      final Matcher m = ANSWERCOMMENT_ID_PATTERN.matcher(AnswerCommentIdString);
+      if (m.find()) {
+        return m.group(1);
+      }
+    }
+    return null;
+  }
+
+  public String getQuestionId() { return questionId; }
+
+  public String getAuthorName() {
+    final Elements elements = answerCommentElement.select("div.cdByLine a");
+    if (!elements.isEmpty()) {
+      return answerCommentElement.select("div.cdByLine a").first().text();
+    }
+    return null;
+  }
+
+  public String getAuthorId() {
+    final String authorIdString = answerCommentElement.select("div.cdByLine a").attr("href");
+    final Matcher m = AUTHOR_ID_PATTERN.matcher(authorIdString);
+    if (m.find()) {
+      return m.group(1);
+    }
+    return null;
+  }
+
+  public Date getPostDate() {
+    final Date date;
+    final String dateString = answerCommentElement.select("div.cdByLine").text().replace(",", "").trim();
+    final Matcher m = DATE_PATTERN.matcher(dateString);
+    if (m.find()) {
+      try {
+        date = DATE_FORMAT.parse(m.group(1));
+        return date;
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
+    }
+    return null;
+  }
+
+  public String getAnswerText() {
+    return answerText;
+  }
+
+  public String getAnswerCommentText() {
+    return answerCommentElement.select("div.cdCommentText").text();
+  }
+
+  public Map<String, Object> asMap() {
+    final Map<String, Object> answerCommentDoc = new LinkedHashMap<>();
+    answerCommentDoc.put("Answer Comment ID", getAnswerCommentId());
+    answerCommentDoc.put("Question ID", getQuestionId());
+    answerCommentDoc.put("Author Name", getAuthorName());
+    answerCommentDoc.put("Author ID", getAuthorId());
+    answerCommentDoc.put("Post Date", getPostDate());
+    answerCommentDoc.put("Answer Text", getAnswerText());
+    answerCommentDoc.put("Answer Comment Text", getAnswerCommentText());
+    return answerCommentDoc;
+  }
+
+}
--- crawler/src/main/java/sg/edu/smu/webmining/crawler/processor/AnswerCommentPageProcessor.java
+++ crawler/src/main/java/sg/edu/smu/webmining/crawler/processor/AnswerCommentPageProcessor.java
@@ -4,7 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import sg.edu.smu.webmining.crawler.datatype.Question;
+import sg.edu.smu.webmining.crawler.datatype.AnswerComment;
 import sg.edu.smu.webmining.crawler.downloader.nio.ProxyNHttpClientDownloader;
 import sg.edu.smu.webmining.crawler.mongodb.GeneralMongoDBManager;
 import sg.edu.smu.webmining.crawler.pipeline.GeneralMongoDBPipeline;
@@ -17,19 +17,15 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by hwei on 7/12/2016.
+ * Created by hwei on 15/12/2016.
  */
-public class QuestionPageProcessor implements PageProcessor {
+public class AnswerCommentPageProcessor implements PageProcessor{
 
-  private static final Pattern QUESTION_ID_PATTERN = Pattern.compile("/-/([a-zA-Z0-9]+)/");
-  private static final Pattern CATEGORY_URL_PATTERN = Pattern.compile("/ask/questions/");
-  private static final Pattern QUESTION_URL_PATTERN = Pattern.compile("/forum/");
+  private static final Pattern QUESTION_ID_PATTERN = Pattern.compile("/(T[a-zA-Z0-9]+)/");
 
   private final Site site = Site.me()
       .setCycleRetryTimes(Integer.MAX_VALUE)
@@ -37,50 +33,39 @@ public class QuestionPageProcessor implements PageProcessor {
       .setRetryTimes(3)
       .setCharset("UTF-8");
 
-  private List<String> extractAndAddUrls(Document doc) {
-    List<String> urlList = new ArrayList<>();
-    Elements result = doc.select("div.a-section.askTeaserQuestions a.a-link-normal");
-    for (Element e : result) {
-      final String urlString = e.attr("href");
-      if (!urlString.isEmpty()) {
-        urlList.add(urlString);
+  public String getQuestionId(String questionIdLink) {
+    final Matcher m = QUESTION_ID_PATTERN.matcher(questionIdLink);
+    if (m.find()) {
+      return m.group(1);
+    }
+    return null;
+  }
+
+  public String getNextLink(Elements paginationElements) {
+    if (!paginationElements.isEmpty()) {
+      if (paginationElements.last().text().equals("Next ›")) {
+        return paginationElements.last().attr("href");
       }
     }
-    return urlList;
+    return null;
   }
-
-  private String extractNextUrl(Document doc) {
-    return doc.select("ul.a-pagination li.a-last a").attr("href");
-  }
-
-  private Boolean isCategoryPage(String pageURL) {
-    return CATEGORY_URL_PATTERN.matcher(pageURL).find();
-  }
-
-  private Boolean isQuestionPage(String pageURL) {
-    return QUESTION_URL_PATTERN.matcher(pageURL).find();
-  }
-
 
   @Override
   public void process(Page page) {
-    final String pageURL = page.getUrl().toString();
     final Document doc = Jsoup.parse(page.getRawText(), "https://www.amazon.com");
-    if (isCategoryPage(pageURL)) {
-      page.addTargetRequests(extractAndAddUrls(doc));
-      page.addTargetRequest(extractNextUrl(doc));
-      page.setSkip(true);
-    } else if (isQuestionPage(pageURL)) {
-      final Element questionElement = doc.select("div.cdAnswerListHeader").first();
-      final Matcher m = QUESTION_ID_PATTERN.matcher(pageURL);
-      String questionID = null;
-      if (m.find()) {
-        questionID = m.group(1);
-      }
-      final Question question = new Question(questionID, questionElement);
-      page.putField(questionID, question.asMap());
-      System.out.println(question.asMap());
+    final String nextLink = getNextLink(doc.select("div.cdPageSelectorPagination a"));
+    if(nextLink != null) {
+      page.addTargetRequest(nextLink);
     }
+    final Elements answerCommentElements = doc.select("div.cdCommentList div.cdComment");
+    final String answerText = doc.select("span#long_cdAnswerDisplay").text().trim();
+    final String questionId = getQuestionId(doc.select("span.cdQuestionLink a").attr("href"));
+    for (Element element: answerCommentElements) {
+      final AnswerComment ansComment = new AnswerComment(element, questionId, answerText);
+      page.putField(ansComment.getAnswerCommentId(), ansComment.asMap());
+    }
+
+
   }
 
   @Override
@@ -89,7 +74,8 @@ public class QuestionPageProcessor implements PageProcessor {
   }
 
   public static void main(String[] args) {
-    final String testUrl = "https://www.amazon.com/ask/questions/asin/B003EM8008/1/ref=ask_ql_psf_ql_hza?sort=HELPFUL&isAnswered=true";
+
+    final String testUrl = "https://www.amazon.com/gp/forum/cd/discussion.html/ref=cm_cd_NOREF?ie=UTF8&asin=B003EM8008&cdForum=FxGU9L9IR0HOWY&cdPage=1&cdThread=Tx2QRCXY0HAAYA";
 
     DynamicProxyProviderTimerWrap provider = new DynamicProxyProviderTimerWrap(
         new DynamicProxyProvider()
@@ -100,10 +86,10 @@ public class QuestionPageProcessor implements PageProcessor {
     try {
       provider.startAutoRefresh();
 
-      try (final GeneralMongoDBManager manager = new GeneralMongoDBManager("localhost", 27017, "QuestionPage", "content")) {
+      try (final GeneralMongoDBManager manager = new GeneralMongoDBManager("localhost", 27017, "AnswerCommentPage", "content")) {
         try (final ProxyNHttpClientDownloader downloader = new ProxyNHttpClientDownloader(provider)) {
 
-          Spider spider = Spider.create(new QuestionPageProcessor())
+          Spider spider = Spider.create(new AnswerCommentPageProcessor())
               .setDownloader(downloader)
               .addPipeline(new GeneralMongoDBPipeline(manager))
               .addUrl(testUrl)
@@ -122,4 +108,5 @@ public class QuestionPageProcessor implements PageProcessor {
       provider.stopAutoRefresh();
     }
   }
+
 }
